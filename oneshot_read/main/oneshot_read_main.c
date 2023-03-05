9c1983520f80b0d01aa653dd467ebd61bf3eb89b
minh2002sn <minhphngnht@gmail.com>
2023-03-05 23:05:03 +0700

    Done running ADC example

---
+++ oneshot_read/main/oneshot_read_main.c
@@ -0,0 +1,205 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "soc/soc_caps.h"
+#include "esp_log.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
+
+#include "subdriver_gpio.h"
+#define GPIO_BUTTON_PIN         9
+#define GPIO_RGB_RED_PIN        3
+#define GPIO_RGB_GREEN_PIN      4
+#define GPIO_RGB_BLUE_PIN       5
+#define GPIO_ORANGE_LED         18
+#define GPIO_WHITE_LED          19
+
+const static char *TAG = "EXAMPLE";
+
+/*---------------------------------------------------------------
+        ADC General Macros
+---------------------------------------------------------------*/
+//ADC1 Channels
+#if CONFIG_IDF_TARGET_ESP32
+#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_4
+#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_5
+#else
+#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_2
+#define EXAMPLE_ADC1_CHAN1          ADC_CHANNEL_3
+#endif
+
+#if (SOC_ADC_PERIPH_NUM >= 2) && !CONFIG_IDF_TARGET_ESP32C3
+/**
+ * On ESP32C3, ADC2 is no longer supported, due to its HW limitation.
+ * Search for errata on espressif website for more details.
+ */
+#define EXAMPLE_USE_ADC2            1
+#endif
+
+#if EXAMPLE_USE_ADC2
+//ADC2 Channels
+#if CONFIG_IDF_TARGET_ESP32
+#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
+#else
+#define EXAMPLE_ADC2_CHAN0          ADC_CHANNEL_0
+#endif
+#endif  //#if EXAMPLE_USE_ADC2
+
+#define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_11
+
+static int adc_raw[2][10];
+static int voltage[2][10];
+static bool example_adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle);
+static void example_adc_calibration_deinit(adc_cali_handle_t handle);
+
+static EventGroupHandle_t led_status_event_group;
+#define LED_STATE_NORMAL            (1 << 0)
+#define LED_STATE_DANGER            (1 << 1)
+
+void led_status_handle_task()
+{
+    SUBDRIVER_GPIO_OutputInit(GPIO_RGB_RED_PIN);
+    SUBDRIVER_GPIO_OutputInit(GPIO_RGB_GREEN_PIN);
+    SUBDRIVER_GPIO_SetState(GPIO_RGB_RED_PIN, 0);
+    SUBDRIVER_GPIO_SetState(GPIO_RGB_GREEN_PIN, 1);
+    led_status_event_group = xEventGroupCreate();
+    while(1)
+    {
+        EventBits_t uxBits = xEventGroupWaitBits(led_status_event_group, 0x00FFFFFF, pdTRUE, pdFALSE, portMAX_DELAY);
+        if(uxBits & LED_STATE_NORMAL)
+        {
+            SUBDRIVER_GPIO_SetState(GPIO_RGB_RED_PIN, 0);
+            SUBDRIVER_GPIO_SetState(GPIO_RGB_GREEN_PIN, 1);
+        }
+        if(uxBits & LED_STATE_DANGER)
+        {
+            SUBDRIVER_GPIO_SetState(GPIO_RGB_RED_PIN, 1);
+            SUBDRIVER_GPIO_SetState(GPIO_RGB_GREEN_PIN, 0);
+        }
+    }
+}
+
+void app_main(void)
+{
+    xTaskCreate(led_status_handle_task, "led_status_handle_task", 2048, NULL, 13, NULL);
+    //-------------ADC1 Init---------------//
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+    //-------------ADC1 Config---------------//
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = EXAMPLE_ADC_ATTEN,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));
+    // ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN1, &config));
+
+    //-------------ADC1 Calibration Init---------------//
+    adc_cali_handle_t adc1_cali_handle = NULL;
+    bool do_calibration1 = example_adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC_ATTEN, &adc1_cali_handle);
+
+    while (1) {
+        ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
+        ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
+        if (do_calibration1) {
+            ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_handle, adc_raw[0][0], &voltage[0][0]));
+            ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, voltage[0][0]);
+        }
+
+        if(voltage[0][0] < 1500)
+        {
+            printf("normal\n");
+            xEventGroupSetBits(led_status_event_group, LED_STATE_NORMAL);
+        }
+        else
+        {
+            printf("danger\n");
+            xEventGroupSetBits(led_status_event_group, LED_STATE_DANGER);
+        }
+        
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+    }
+
+    //Tear Down
+    ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
+    if (do_calibration1) {
+        example_adc_calibration_deinit(adc1_cali_handle);
+    }
+}
+
+
+/*---------------------------------------------------------------
+        ADC Calibration
+---------------------------------------------------------------*/
+static bool example_adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle)
+{
+    adc_cali_handle_t handle = NULL;
+    esp_err_t ret = ESP_FAIL;
+    bool calibrated = false;
+
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+    if (!calibrated) {
+        ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
+        adc_cali_curve_fitting_config_t cali_config = {
+            .unit_id = unit,
+            .atten = atten,
+            .bitwidth = ADC_BITWIDTH_DEFAULT,
+        };
+        ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
+        if (ret == ESP_OK) {
+            calibrated = true;
+        }
+    }
+#endif
+
+#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+    if (!calibrated) {
+        ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+        adc_cali_line_fitting_config_t cali_config = {
+            .unit_id = unit,
+            .atten = atten,
+            .bitwidth = ADC_BITWIDTH_DEFAULT,
+        };
+        ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
+        if (ret == ESP_OK) {
+            calibrated = true;
+        }
+    }
+#endif
+
+    *out_handle = handle;
+    if (ret == ESP_OK) {
+        ESP_LOGI(TAG, "Calibration Success");
+    } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
+        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
+    } else {
+        ESP_LOGE(TAG, "Invalid arg or no memory");
+    }
+
+    return calibrated;
+}
+
+static void example_adc_calibration_deinit(adc_cali_handle_t handle)
+{
+#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
+    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
+
+#elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
+    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
+#endif
+}
